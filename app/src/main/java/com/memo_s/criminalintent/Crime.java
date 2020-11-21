91298f38db2491cbb8d7f06ca61ab57c39f5eec9
Memo98171 <p.szopian@studenti.unisa.it>
2020-11-21 18:45:26 +0100

    CriminalIntent application

---
+++ app/src/main/java/com/memo_s/criminalintent/Crime.java
@@ -0,0 +1,54 @@
+package com.memo_s.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+    private boolean mRequiresPolice;
+
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new Date();
+
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        this.mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        this.mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        this.mSolved = solved;
+    }
+
+    public boolean isRequiresPolice(){
+        return  mRequiresPolice;
+    }
+
+    public void setRequiresPolice(boolean requiresPolice){
+        mRequiresPolice = requiresPolice;
+    }
+}
