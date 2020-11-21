df0e5f786102c4e901eb77996a97803fae88e139
Memo98171 <p.szopian@studenti.unisa.it>
2020-11-21 21:17:49 +0100

    Implemeting CrimePagerActivity

--- app/src/main/java/com/memo_s/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/memo_s/criminalintent/CrimeListFragment.java
@@ -112,7 +112,7 @@ public class CrimeListFragment extends Fragment {
         @Override
         public void onClick(View view){
             listItemPosition = getAdapterPosition();
-            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            Intent intent = CrimePagerActivity.newIntent(getActivity(), mCrime.getId());
             startActivityForResult(intent, REQUEST_CRIME);
         }
     }
